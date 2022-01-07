efc2c66adff0e4e8d2529c22148a89b8dd0205ea
femmyte <sanyaoluadefemi@gmail.com>
2022-01-07 15:08:25 +0800

    worked on the third component which consist of the product list

--- swith/src/App.js
+++ swith/src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from "./components/Header"
-import SectionOne from "./components/SectionOne"
-import SectionTwo from "./components/SectionTwo"
-import SectionThree from "./components/SectionThree"
-import Footer from "./components/Footer"
-import Login from "./components/Login"
+import Header from './components/Header'
+import SectionOne from './components/SectionOne'
+import SectionTwo from './components/SectionTwo'
+import SectionThree from './components/SectionThree'
+import Footer from './components/Footer'
+import Login from './components/Login'
 
 function App() {
   return (
-
-    <div className="App">
+    <div className='App'>
       {/* 
       <Header/>
       <SectionOne/>
@@ -19,13 +18,18 @@ function App() {
       <Footer/>
       <Login/> */}
 
-      <header className="card">
-        
-        <div className="card-title text-primary mt-4">SWITH Global Market Hub</div>
-        <p className="card-body"><b>Project Topic: </b>A startup that want to make it easier for companies to purchase goods from China.</p>
+      <header className='card'>
+        <div className='card-title text-primary mt-4'>
+          SWITH Global Market Hub
+        </div>
+        <p className='card-body'>
+          <b>Project Topic: </b>A startup that want to make it easier for
+          companies to purchase goods from China.
+        </p>
       </header>
+      <SectionThree />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ swith/src/components/data.js
@@ -0,0 +1,68 @@
+const products = [
+  {
+    id: 1,
+    name: 'vintage typewriter',
+    image:
+      'https://www.subhe.com/blog/wp-content/uploads/2019/10/Ecommerce-Photo-Editing.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 2,
+    name: 'vintage typewriter',
+    image:
+      'https://assets-global.website-files.com/600fe6e1ff56087409a9f096/605b5a558848493df14d2d13_ecommerce-product-photography.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 3,
+    name: 'vintage typewriter',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQR8zzI0oNTZc3Rpstzt_Rfae2CTcieveBoK1wk_FpgRYZ0tLyV-0rd7xthwhTWtbXwCk&usqp=CAU',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 4,
+    name: 'vintage typewriter',
+    image:
+      'https://assets-global.website-files.com/600fe6e1ff56087409a9f096/605b5a558848493df14d2d13_ecommerce-product-photography.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 5,
+    name: 'vintage typewriter',
+    image:
+      'https://www.subhe.com/blog/wp-content/uploads/2019/10/Ecommerce-Photo-Editing.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 6,
+    name: 'vintage typewriter',
+    image:
+      'https://assets-global.website-files.com/600fe6e1ff56087409a9f096/605b5a558848493df14d2d13_ecommerce-product-photography.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 7,
+    name: 'vintage typewriter',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQR8zzI0oNTZc3Rpstzt_Rfae2CTcieveBoK1wk_FpgRYZ0tLyV-0rd7xthwhTWtbXwCk&usqp=CAU',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+  {
+    id: 8,
+    name: 'vintage typewriter',
+    image:
+      'https://assets-global.website-files.com/600fe6e1ff56087409a9f096/605b5a558848493df14d2d13_ecommerce-product-photography.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    rate: 4,
+  },
+]
+
+export default products
