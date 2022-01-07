09fc7563df8a17d24e1635617125150365fdcb28
Akande Oluwatosin <aktosin1@gmail.com>
2022-01-07 23:29:19 +0100

    the little i worked on tonight

--- swith/src/App.js
+++ swith/src/App.js
@@ -1,17 +1,16 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header'
-import SectionOne from './components/SectionOne'
-import SectionTwo from './components/SectionTwo'
-import SectionThree from './components/SectionThree'
-import Footer from './components/Footer'
-import Login from './components/Login'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import SectionThree from "./components/SectionThree";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
 
 function App() {
   return (
-      
-    <div className='App'>
-      <SectionOne/>
+    <div className="App">
+      <SectionOne />
       {/* 
       <Header/>
       
@@ -21,18 +20,19 @@ function App() {
       <Login/> 
       */}
 
-      <header className='card'>
-        <div className='card-title text-primary mt-4'>
+      <header className="card">
+        <div className="card-title text-primary mt-4">
           SWITH Global Market Hub
         </div>
-        <p className='card-body'>
+        <p className="card-body">
           <b>Project Topic: </b>A startup that want to make it easier for
           companies to purchase goods from China.
         </p>
       </header>
+      <SectionTwo />
       <SectionThree />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ swith/src/components/SectionTwo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const SectionTwo = () => {
+  return (
+    <div
+      className="px-4 flex justify-between"
+      style={{ backgroundColor: "#062B46", color: "#fff" }}
+    >
+      <div className="flex" style={{ backgroundColor: "#1E96A6 " }}>
+        <h3>Latest Products in demand</h3>
+      </div>
+      <div>
+        <p>we have what you are looking for</p>
+        <p>enjoy comfort in finding your product with us</p>
+        <p>swith hub limited</p>
+      </div>
+      <div style={{ backgroundColor: "#1e96a6" }}>
+        <h3>Testimonial</h3>
+      </div>
+    </div>
+  );
+};
+
+export default SectionTwo;
